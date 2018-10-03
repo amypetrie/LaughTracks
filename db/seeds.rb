401f484cd80c386d78817ea19ed186a4e153433e
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-10-03 12:49:40 -0600

    setup for simplecov testing completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
+require './app/models/comedian.rb'
+require './app/models/special.rb'
+require 'activerecord-import/base'
 
+comedian = Comedian.create(...)
+special_1 = comedian.specials.create(...)
+special_2 = comedian.special.create(...)
+
+puts "added #{comedian.specials.count} specials for #{comedian.name}."
